0479a4c755e13767aedfcd38aaa91222f8664f72
matheusv11 <matheussales360@hotmail.com>
2020-07-25 23:05:37 -0300

    ok

--- src/services/connection.js
+++ src/services/connection.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const connection= axios.create({
-    baseURL: 'http://localhost:3030'
-    // baseURL: 'https://backend-assifpi.herokuapp.com'
+    // baseURL: 'http://localhost:3030'
+    baseURL: 'https://backend-assifpi.herokuapp.com'
 });
 
 export default connection;
