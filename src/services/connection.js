f6796220e8a79048e1ec83f153cc4c9ad0e06f2c
matheusv11 <matheussales360@hotmail.com>
2020-09-12 23:49:23 -0300

    OK

--- src/services/connection.js
+++ src/services/connection.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const connection= axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3030',
+    baseURL: process.env.REACT_APP_API_URL,
     // baseURL: 'https://backend-assifpi.herokuapp.com'
 });
 
